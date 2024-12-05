eeb7bd3ca57a87b167ac36fa29ee84a612a0d979
Alan Reis <alanreisanjo@gmail.com>
2024-12-05 15:15:35 -0300

    Nem eu sei

--- src/utils/calculateIndividualValues.ts
+++ src/utils/calculateIndividualValues.ts
@@ -1,141 +1,136 @@
-// import { PokemonNatures } from "@/storage";
-// import type {
-//   BaseStats,
-//   EffortValues,
-//   IndividualValues,
-//   IndividualValueType,
-//   Level,
-//   Natures,
-//   PokemonData,
-//   PokemonStats,
-// } from "@/types";
+import { PokemonNatures } from "@/storage";
+import type {
+  BaseStats,
+  EffortValues,
+  IndividualValues,
+  IndividualValueType,
+  Level,
+  Natures,
+  PokemonData,
+  PokemonStats,
+} from "@/types";
+import { calculateCurrentStats } from "./calculateCurrentStats";
 
-// export function calculateIndividualValues(
-//   level: Level,
-//   totalStats: PokemonStats,
-//   baseStats: BaseStats,
-//   effortValues: EffortValues,
-//   nature: Natures
-// ): IndividualValues {
-//   const pokemonNature = PokemonNatures[nature];
+export function calculateIndividualValues(
+  level: Level,
+  totalStats: PokemonStats,
+  baseStats: BaseStats,
+  effortValues: EffortValues,
+  nature: Natures
+): IndividualValues {
+  const pokemonNature = PokemonNatures[nature];
 
-//   const pokemonIndividualValues: IndividualValues = {
-//     HP: 0,
-//     Attack: 0,
-//     Defense: 0,
-//     SpecialAttack: 0,
-//     SpecialDefense: 0,
-//     Speed: 0,
-//   };
+  const pokemonIndividualValues: IndividualValues = {
+    HP: 0,
+    Attack: 0,
+    Defense: 0,
+    SpecialAttack: 0,
+    SpecialDefense: 0,
+    Speed: 0,
+  };
 
-//   for (const currentStat of Object.keys(pokemonIndividualValues) as Array<
-//     keyof PokemonStats
-//   >) {
-//     const natureEffectAtCurrentStat =
-//       pokemonNature.increased === currentStat
-//         ? 1.1
-//         : pokemonNature.decreased === currentStat
-//         ? 0.9
-//         : 1.0;
+  for (const currentStat of Object.keys(pokemonIndividualValues) as Array<
+    keyof PokemonStats
+  >) {
 
-//     if (currentStat === "HP") {
-//       pokemonIndividualValues[currentStat] = (100 *
-//         ((totalStats[currentStat] - level - 10) / level) -
-//         2 * baseStats[currentStat] -
-//         Math.floor(effortValues[currentStat] / 4)) as IndividualValueType;
+    const natureEffectAtCurrentStat =
+      pokemonNature.increased === currentStat
+        ? 1.1
+        : pokemonNature.decreased === currentStat
+        ? 0.9
+        : 1.0;
 
-//       if (pokemonIndividualValues[currentStat] < 0) {
-//         pokemonIndividualValues[currentStat] = 0;
-//       }
+    const totalStat = totalStats[currentStat];
+    const ev = effortValues[currentStat];
+    const baseStat = baseStats[currentStat];
 
-//       continue;
-//     }
+    if (currentStat === "HP") {
+      pokemonIndividualValues[currentStat] = (Math.floor(
+        ((totalStat - level - 10) * 100) / level
+      ) -
+        2 * baseStat -
+        Math.floor(ev / 4)) as IndividualValueType;
 
-//     pokemonIndividualValues[currentStat] = Math.ceil(
-//       (100 * (totalStats[currentStat] / natureEffectAtCurrentStat - 5)) /
-//         level -
-//         2 * baseStats[currentStat] -
-//         Math.floor(effortValues[currentStat] / 4)
-//     ) as IndividualValueType;
+      continue;
+    }
 
-//     if (pokemonIndividualValues[currentStat] < 0) {
-//       pokemonIndividualValues[currentStat] = 0;
-//     }
-//   }
+    pokemonIndividualValues[currentStat] = (Math.floor(
+      ((Math.floor(totalStat / natureEffectAtCurrentStat) - 5) * 100) / level
+    ) -
+      Math.floor(ev / 4) -
+      2 * baseStat) as IndividualValueType;
+  }
 
-//   return pokemonIndividualValues;
-// }
+  return pokemonIndividualValues;
+}
 
-// const SkeledirjeData: PokemonData = {
-//   abilities: [],
-//   name: "Skeledirje",
-//   baseStats: {
-//     HP: 104,
-//     Attack: 75,
-//     Defense: 100,
-//     SpecialAttack: 110,
-//     SpecialDefense: 75,
-//     Speed: 66,
-//   },
-//   breeding: {
-//     eggCycles: 20,
-//     eggGroups: ["Field"],
-//     gender: "Male",
-//   },
-//   moves: [],
-//   number: 911,
-//   sprite: "https://img.pokemondb.net/artwork/avif/skeledirge.avif",
-//   training: {
-//     effortValueYield: [{ stat: "SpecialAttack", value: 3 }],
-//     baseExp: 265,
-//     baseFriendship: 50,
-//     growthRate: "Medium Slow",
-//   },
-//   types: ["Fire", "Ghost"],
-// };
+const SkeledirjeData: PokemonData = {
+  abilities: [],
+  name: "Skeledirje",
+  baseStats: {
+    HP: 104,
+    Attack: 75,
+    Defense: 100,
+    SpecialAttack: 110,
+    SpecialDefense: 75,
+    Speed: 66,
+  },
+  breeding: {
+    eggCycles: 20,
+    eggGroups: ["Field"],
+    gender: "Male",
+  },
+  moves: [],
+  number: 911,
+  sprite: "https://img.pokemondb.net/artwork/avif/skeledirge.avif",
+  training: {
+    effortValueYield: [{ stat: "SpecialAttack", value: 3 }],
+    baseExp: 265,
+    baseFriendship: 50,
+    growthRate: "Medium Slow",
+  },
+  types: ["Fire", "Ghost"],
+};
 
-// // const SkeledirjeSchema: PokemonSchema = {
-// //   data: SkeledirjeData,
-// //   level: 100,
-// //   types: SkeledirjeData.types,
-// //   baseStats: SkeledirjeData.baseStats,
-// //   effortValues: {
-// //     HP: 252,
-// //     Defense: 92,
-// //     SpecialDefense: 164,
-// //     Attack: 0,
-// //     SpecialAttack: 0,
-// //     Speed: 0,
-// //   },
-// //   individualValues: {
-// //     Attack: 24,
-// //     Defense: 31,
-// //     HP: 31,
-// //     SpecialAttack: 31,
-// //     SpecialDefense: 31,
-// //     Speed: 31,
-// //   },
-// //   moves: [null, null, null, null],
-// //   nature: "Bold",
-// //   teraType: "Fairy",
-// // };
+console.log(
+  calculateIndividualValues(
+    100,
+    {
+      HP: 412,
+      Attack: 161,
+      Defense: 284,
+      SpecialAttack: 256,
+      SpecialDefense: 227,
+      Speed: 168,
+    },
+    SkeledirjeData.baseStats,
+    {
+      HP: 252,
+      Defense: 92,
+      SpecialDefense: 164,
+      Attack: 0,
+      SpecialAttack: 0,
+      Speed: 0,
+    },
+    "Bold"
+  )
+);
 
-// console.log(
-//   calculateIndividualValues(
-//     100,
-//     {
-//       HP: 412,
-//       Attack: 161,
-//       Defense: 284,
-//       SpecialAttack: 256,
-//       SpecialDefense: 227,
-//       Speed: 168,
-//     },
-//     SkeledirjeData.baseStats,
-//     SkeledirjeSchema.effortValues,
-//     SkeledirjeSchema.nature
-//   )
-// );
+console.log(calculateCurrentStats(100, SkeledirjeData.baseStats, {
+  HP: 252,
+  Defense: 92,
+  SpecialDefense: 164,
+  Attack: 0,
+  SpecialAttack: 0,
+  Speed: 0,
+}, {
+  HP: 31,
+  Attack: 24,
+  Defense: 31,
+  SpecialAttack: 31,
+  SpecialDefense: 31,
+  Speed: 31
+}, "Bold"))
 
 // const ArmarougeData: PokemonData = {
 //   number: 936,
@@ -165,32 +160,6 @@
 //   types: ["Fire", "Psychic"],
 // };
 
-// // const ArmarougeSchema: PokemonSchema = {
-// //   data: ArmarougeData,
-// //   baseStats: ArmarougeData.baseStats,
-// //   effortValues: {
-// //     Attack: 0,
-// //     Defense: 0,
-// //     HP: 0,
-// //     SpecialAttack: 0,
-// //     SpecialDefense: 0,
-// //     Speed: 0,
-// //   },
-// //   individualValues: {
-// //     Attack: 0,
-// //     Defense: 0,
-// //     HP: 0,
-// //     SpecialAttack: 0,
-// //     SpecialDefense: 0,
-// //     Speed: 0,
-// //   },
-// //   level: 75,
-// //   moves: [null, null, null, null],
-// //   nature: "Docile",
-// //   teraType: "Poison",
-// //   types: ArmarougeData.types,
-// // };
-
 // console.log(
 //   calculateIndividualValues(
 //     75,
@@ -202,8 +171,15 @@
 //       SpecialDefense: 136,
 //       Speed: 140,
 //     },
-//     ArmarougeSchema.baseStats,
-//     ArmarougeSchema.effortValues,
-//     ArmarougeSchema.nature
+//     ArmarougeData.baseStats,
+//     {
+//       Attack: 0,
+//       Defense: 0,
+//       HP: 0,
+//       SpecialAttack: 0,
+//       SpecialDefense: 0,
+//       Speed: 0,
+//     },
+//     "Docile"
 //   )
 // );
---
+++ test.ts
@@ -0,0 +1,20 @@
+/* 
+
+1 IV no LVL 100 = 1 PT
+
+Pikachu Lvl 100 Timid + SPEED; - ATTACK
+EV N;
+
+DIFF(Pika0IV, PikaNIv)
+
+PT = IV / LEVEL
+
+
+31 = 100
+x = 1
+
+Math.floor(iv / level) = DIFF(Pika0IV, PikaNIv)
+iv = (DIFF(Pika0IV, PikaNIv) / level) * 100
+
+0.31 pt by lvl
+*/
